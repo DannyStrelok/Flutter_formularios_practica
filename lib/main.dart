9ff32a68b5fcca0747c88c6b6d8671c18a02e06d
DannyStrelok <hmartindaniel@gmail.com>
2020-12-03 22:51:59 +0100

    Primer commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_validation/src/screens/home_screen.dart';
+import 'package:flutter_form_validation/src/screens/login_screen.dart';
+
+void main() async {
+  // AQUÍ PODEMOS EXTRAER DATOS QUE NECESITEMOS ANTES DE TERMINAR LA SPLASH SCREEN
+  //await Future.delayed(Duration(seconds: 5), () => print('cargado con retraso'));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Formularios',
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => LoginScreen(),
+        'home': (BuildContext context) => HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          _loginBackground(context),
+          _loginForm(context)
+        ],
+      ),
+    );
+  }
+
+  Widget _loginBackground(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+    final backgroundColor =  Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: <Color>[
+            Color.fromRGBO(63, 63, 156, 1),
+            Color.fromRGBO(90, 70, 178, 0.9),
+          ]
+        )
+      ),
+    );
+
+    final circle = Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: Color.fromRGBO(255, 255, 255, 0.05),
+      ),
+    );
+
+    return Stack(
+      children: [
+        backgroundColor,
+        Positioned(
+          top: 90.0,
+            left: 30.0,
+            child: circle
+        ),
+        Positioned(
+            top: -40.0,
+            right: -30.0,
+            child: circle
+        ),
+        Positioned(
+            bottom: -50.0,
+            right: -10.0,
+            child: circle
+        ),
+
+        Positioned(
+            bottom: 120.0,
+            right: 25.0,
+            child: circle
+        ),
+
+        Positioned(
+            bottom: -50.0,
+            left: -20.0,
+            child: circle
+        ),
+
+        Container(
+          padding: EdgeInsets.only(top: 80.0),
+          child: Column(
+            children: [
+              Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0,),
+              SizedBox(height: 10.0, width: double.infinity,),
+              Text('Daniel', style: TextStyle(color: Colors.white, fontSize: 25.0),)
+            ],
+          ),
+        )
+
+      ],
+    );
+
+  }
+
+  Widget _loginForm(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SafeArea(child: Container(height: 200.0,)),
+          Container(
+            width: size.width * 0.85,
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            margin: EdgeInsets.symmetric(vertical: 30.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10.0,
+                  offset: Offset(0.0, 5.0),
+                  spreadRadius: 3.0
+                )
+              ]
+            ),
+            child: Column(
+              children: [
+                Text('Ingreso', style: TextStyle(fontSize: 20.0),),
+                SizedBox(height: 50.0,),
+                _emailField(),
+                SizedBox(height: 20.0,),
+                _passwordField(),
+                SizedBox(height: 30.0,),
+                _submitButton()
+              ],
+            ),
+          ),
+          Text('Recuperar contraseña'),
+          SizedBox(height: 30.0,)
+        ],
+      ),
+    );
+  }
+
+  Widget _emailField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email, color: Colors.deepPurple,),
+          hintText: "username@email.com",
+          labelText: "Email"
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            icon: Icon(Icons.lock, color: Colors.deepPurple,),
+            hintText: "12345678",
+            labelText: "Contraseña"
+        ),
+      ),
+    );
+  }
+
+  Widget _submitButton() {
+    return RaisedButton(
+      onPressed: (){},
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0)
+      ),
+      elevation: 0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: Text('Iniciar sesión'),
+      ),
+    );
+  }
+
+}
